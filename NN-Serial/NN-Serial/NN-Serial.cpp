9de40b3c972a3f2017e9bf8de92c593a951a72c4
Dan Thick <dan.thick@hotmail.co.uk>
2019-12-10 19:48:49 +0000

    Implemented feed forward pass

--- NN-Serial/NN-Serial/NN-Serial.cpp
+++ NN-Serial/NN-Serial/NN-Serial.cpp
@@ -1,20 +1,146 @@
-// NN-Serial.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 #include <iostream>
+#include <time.h>
+//#include "Matrix.cpp"
+
+// Matrix structure. Matrices are stored in row-major order.
+typedef struct {
+	int width;
+	int height;
+	float* elements;
+	int stride;
+} Matrix;
+
+Matrix matMult(const Matrix A, const Matrix B);
+Matrix feedForward(Matrix W1, Matrix W2, Matrix input);
+Matrix sigmoidFunction(Matrix input);
+Matrix train(Matrix W1, Matrix W2, Matrix input);
+Matrix matSub(const Matrix A, const Matrix B);
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	srand(time(NULL));
+	// Defining network variables
+	int noOfInputs = 2; int noOfHiddenNodes = 3; int noOfOutputs = 1;
+
+	// Defining weight matrices, accounting for bias on dimentions
+	Matrix W1, W2;
+	W1.height = noOfHiddenNodes;
+	W1.width = noOfInputs + 1;
+	W1.elements = (float*)malloc(W1.width * W1.height * sizeof(float));
+	W2.height = noOfOutputs;
+	W2.width = noOfHiddenNodes + 1;
+	W2.elements = (float*)malloc(W2.width * W2.height * sizeof(float));
+
+	// Setting weights to random values between 0 and 1 
+	for (int i = 0; i < W1.height; i++)
+		for (int j = 0; j < W1.width; j++)
+			W1.elements[i * W1.width + j] = ((double)rand() / (RAND_MAX + 1.0));
+
+	for (int i = 0; i < W2.height; i++)
+		for (int j = 0; j < W2.width; j++)
+			W2.elements[i * W2.width + j] = ((double)rand() / (RAND_MAX + 1.0));
+
+
+	Matrix input;
+	input.height = 2;
+	input.width = 1;
+	input.elements = (float*)malloc(input.width * input.height * sizeof(float));
+	input.elements[0] = 2.5;
+	input.elements[1] = 4.5;
+
+	Matrix output = feedForward(W1, W2, input);
+
+	for (int i = 0; i < output.height; i++)
+		for (int j = 0; j < output.width; j++)
+			printf("%f", output.elements[0]);
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+Matrix train(Matrix W1, Matrix W2, Matrix input, Matrix target) {
+	// Add bias to the input matrix
+	input.elements[input.height] = 1;
+	// Calculate output from hidden layer
+	Matrix net = matMult(W1, input);
+	// Sigmoid activation function
+	net = sigmoidFunction(net);
+	// Add bias to activation from hidden layer, add update matrix dimensions
+	net.elements[net.height] = 1;
+	net.height++;
+	// Calculate output from last layer
+	net = matMult(W2, net);
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+	Matrix delta3 = matSub(target, net);
+	for (int i = 0; i < delta3.height; i++) {
+		for (int i = 0; i < delta3.width; i++) {
+			delta3.elements[j + i * delta3.width] = -delta3.elements[j + i * delta3.width];
+		}
+	}
+
+
+
+}
+
+
+
+Matrix feedForward(Matrix W1, Matrix W2, Matrix input) {
+	// Add bias to the input matrix
+	input.elements[input.height] = 1;
+	// Calculate output from hidden layer
+	Matrix net = matMult(W1, input);
+	// Sigmoid activation function
+	net = sigmoidFunction(net);
+	// Add bias to activation from hidden layer, add update matrix dimensions
+	net.elements[net.height] = 1;
+	net.height++;
+	// Calculate output from last layer
+	net = matMult(W2, net);
+	return net;
+
+}
+
+Matrix sigmoidFunction(Matrix input) {
+	for (int i = 0; i < input.height; i++) {
+		for (int j = 0; j < input.width; j++) {
+			input.elements[i * input.width + j] = 1 / (1+(exp(-input.elements[i * input.width + j])));
+		}
+	}
+	return input;
+}
+
+// Function to complete a matrix mulitplication on the host
+Matrix matMult(const Matrix A, const Matrix B) {
+	Matrix C;
+	C.height = A.height;
+	C.width = B.width;
+	C.elements = (float*)malloc(C.width * C.height * sizeof(float));
+
+	for (int i = 0; i < A.height; i++) {
+		for (int j = 0; j < B.width; j++) {
+			C.elements[j + i * B.width] = 0;
+
+			for (int k = 0; k < A.width; k++) {
+				C.elements[j + i * B.width] += A.elements[k + i * A.width] * B.elements[j + k * B.width];
+			}
+		}
+	}
+	return C;
+}
+
+Matrix matSub(const Matrix A, const Matrix B) {
+	Matrix C;
+	C.height = A.height;
+	C.width = A.width;
+	C.elements = (float*)malloc(C.width * C.height * sizeof(float));
+
+	for (int i = 0; i < C.height; i++) {
+		for (int j = 0; j < C.width; j++) {
+			C.elements[j + i * C.width] = A.elements[j + i * C.width] - B.elements[j + i * C.width];
+		}
+	}
+	
+}
