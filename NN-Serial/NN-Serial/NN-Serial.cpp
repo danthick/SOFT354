4f658b0aea7e4fb5612adb32fa88c750f53de0ec
Dan Thick <dan.thick@hotmail.co.uk>
2019-12-17 14:34:51 +0000

    Re-written network from first principles. Refactored code add implemented my own matrix library.

--- NN-Serial/NN-Serial/NN-Serial.cpp
+++ NN-Serial/NN-Serial/NN-Serial.cpp
@@ -4,236 +4,149 @@
 #include <iostream>
 #include <time.h>
 #include <vector>
+#include <cstddef>
+#include <stddef.h>
+#include "Matrix.cpp"
 #pragma warning(disable:4996)
-//#include "Matrix.cpp"
 
-// Matrix structure. Matrices are stored in row-major order.
-typedef struct {
-	int width;
-	int height;
-	float* elements;
-	int stride;
-} Matrix;
+struct Network {
+	Matrix W1;
+	Matrix W2;
+	int noOfInputs;
+	int noOfHidden;
+	int noOfOutputs;
 
-Matrix matMult(const Matrix A, const Matrix B);
-Matrix feedForward(Matrix W1, Matrix W2, Matrix input);
-Matrix sigmoidFunction(Matrix input);
-std::vector<Matrix> train(Matrix W1, Matrix W2, Matrix input, Matrix target);
-Matrix matSub(const Matrix A, const Matrix B);
-Matrix matTranspose(const Matrix A);
+	Network(int inputs, int hidden, int outputs) {
+		this->noOfInputs = inputs;
+		this->noOfHidden = hidden;
+		this->noOfOutputs = outputs;
+	}
+};
+
+Matrix readInData(int height, int width) {
+	Matrix input;
+	input.height = 150;
+	input.width = 5;
+	input.elements = (float*)malloc(input.width * input.height * sizeof(float));
+
+	FILE* fp;
+	size_t count = 0;
+
+	fp = fopen("iris.csv", "r");
+	if (fp == NULL) {
+		fprintf(stderr, "Error reading file\n");
+	}
+
+	while (fscanf(fp, "%f,%f,%f,%f,%f\n", &input.elements[count * input.width + 0], &input.elements[count * input.width + 1], &input.elements[count * input.width + 2], &input.elements[count * input.width + 3], &input.elements[count * input.width + 4]) == 5) {
+		count++;
+	}
+	fclose(fp);
+
+	return input;
+}
+
+Network generateWeights(Network network) {
+	// Setting up weight matracies
+	network.W1.height = network.noOfHidden;
+	network.W1.width = network.noOfInputs;
+	network.W1.elements = (float*)malloc(network.W1.width * network.W1.height * sizeof(float));
+	network.W2.height = network.noOfOutputs;
+	network.W2.width = network.noOfHidden;
+	network.W2.elements = (float*)malloc(network.W2.width * network.W2.height * sizeof(float));
+
+	// Setting weights to random values between 0 and 1 
+	network.W1 = matRand(network.W1);
+	network.W2 = matRand(network.W2);
+
+	return network;
+}
+
+Matrix feedForward(Network network, Matrix input) {
+	// Generating hidden ouputs
+	Matrix net = matMult(network.W1, input);
+	//Matrix hiddenOutputs = matAdd(hiddenInputs, network.B1);
+	Matrix hiddenOutputs = activation(net);
+	// Generating outputs
+	Matrix outputs = matMult(network.W2, hiddenOutputs);
+	//out = matAdd(out, network.B2);
+	outputs = activation(outputs);
+	return outputs;
+}
+
+Network train(Network network, Matrix input, Matrix target) {
+	// FEEDFORWARD
+	// Generating hidden ouputs
+	Matrix net = matMult(network.W1, input);
+	Matrix hiddenOutputs = activation(net);
+	// Generating outputs
+	Matrix outputs = matMult(network.W2, hiddenOutputs);
+	outputs = activation(outputs);
+
+	// Calculate output error
+	Matrix outputError = matSub(target, outputs);
+
+
+	// START BACKPROP
+	// Calculate hidden error
+	Matrix W2T = matTranspose(network.W2);
+	Matrix hiddenError = matScale(W2T, outputError.elements[0]);
+
+	// Calculate output gradients
+	Matrix gradientOut = derivative(outputs);
+	gradientOut = matElementMult(gradientOut, outputError);
+	gradientOut = matScale(gradientOut, 0.1);
+
+	//Calculate hidden gradients
+	Matrix gradientHidden = derivative(hiddenOutputs);
+	gradientHidden = matElementMult(gradientHidden, hiddenError);
+	gradientHidden = matScale(gradientHidden, 0.1);
+
+	Matrix hiddenOutputsT = matTranspose(hiddenOutputs);
+	Matrix deltaOutput = matMult(gradientOut, hiddenOutputsT);
+	network.W2 = matAdd(network.W2, deltaOutput);
+
+	Matrix inputsT = matTranspose(input);
+	Matrix deltaHidden = matMult(gradientHidden, inputsT);
+	network.W1 = matAdd(network.W1, deltaHidden);
+
+	free(net.elements); free(hiddenOutputs.elements); free(outputs.elements);
+	free(outputError.elements); free(W2T.elements); free(hiddenError.elements);
+	free(gradientOut.elements); free(gradientHidden.elements); free(hiddenOutputsT.elements);
+	free(deltaOutput.elements); free(inputsT.elements); free(deltaHidden.elements);
+
+	return network;
+}
+
+
 
 
 int main()
 {
 	srand(time(NULL));
-	// Defining network variables
-	int noOfInputNodes = 2; int noOfHiddenNodes = 3; int noOfOutputs = 1;
+	// Read in data from CSV file
+	Matrix data = readInData(150, 5);
+	// Define network parameters
+	Network network(4,5,1);
+	// Generate intial weights for network
+	network = generateWeights(network);
 
-	// Defining weight matrices, accounting for bias on dimentions
-	Matrix W1, W2;
-	W1.height = noOfHiddenNodes;
-	W1.width = noOfInputNodes;
-	//W1.width = noOfInputs + 1;
-	W1.elements = (float*)malloc(W1.width * W1.height * sizeof(float));
-	W2.height = noOfOutputs;
-	W2.width = noOfHiddenNodes;
-	//W2.width = noOfHiddenNodes + 1;
-	W2.elements = (float*)malloc(W2.width * W2.height * sizeof(float));
+	Matrix input(4, 1);
+	Matrix target(1, 1);
 
-	// Setting weights to random values between 0 and 1 
-	for (int i = 0; i < W1.height; i++)
-		for (int j = 0; j < W1.width; j++)
-			W1.elements[i * W1.width + j] = ((double)rand() / (RAND_MAX + 1.0));
-
-	for (int i = 0; i < W2.height; i++)
-		for (int j = 0; j < W2.width; j++)
-			W2.elements[i * W2.width + j] = ((double)rand() / (RAND_MAX + 1.0));
-
-
-	float sepal_legnth = 0.0;
-	float sepal_width = 0.0;
-	float petal_legnth = 0.0;
-	float petal_width = 0.0;
-	float type = 0.0;
-
-	FILE* fp = fopen("iris.csv", "r");
-
-	int i = 0;
-	int row = 150;
-	float data[5][150];
-	char line[150];
-	while (fgets(line, 150, fp) && (i < row))
-	{
-		// double row[ssParams->nreal + 1];
-		char* tmp = strdup(line);
-
-		int j = 0;
-		const char* tok;
-		for (tok = strtok(line, ","); tok && *tok; j++, tok = strtok(NULL, "\n"))
-		{
-			data[i][j] = atof(tok);
-			printf("%f", data[i][j]);
-		}
-		printf("\n");
-
-		free(tmp);
-		i++;
-	}
-
-
-	Matrix input;
-	input.height = 2;
-	input.width = 1;
-	input.elements = (float*)malloc(input.width * input.height * sizeof(float));
-	input.elements[0] = 2.5;
-	input.elements[1] = 4.5;
-	Matrix target;
-	target.height = 2;
-	target.width = 1;
-	target.elements = (float*)malloc(target.width * target.height * sizeof(float));
-	target.elements[0] = 20.2345678;
-	target.elements[1] = 9;
-
-	
-	std::vector<Matrix> weights = train(W1, W2, input, input);
-	for (int i = 0; i < 5000; i++) {
-		weights = train(weights[0], weights[1], input, target);
-	}
-
-	Matrix output = feedForward(weights[0], weights[1], input);
-
-}
-
-std::vector<Matrix> train(Matrix W1, Matrix W2, Matrix input, Matrix target) {
-	// Add bias to the input matrix
-	input.elements[input.height] = 1;
-	// Calculate output from hidden layer
-	Matrix net = matMult(W1, input);
-	// Sigmoid activation function
-	Matrix a2 = sigmoidFunction(net);
-	// Add bias to activation from hidden layer, and update matrix dimensions
-	Matrix a2Hat = a2;
-	a2Hat.elements[net.height] = 1;
-	a2Hat.height++;
-	// Calculate output from last layer
-	Matrix o = matMult(W2, net);
-
-
-	// Performing matrix subtraction
-	Matrix delta3 = matSub(target, o);
-	// Changing all values to negative
-	for (int i = 0; i < delta3.height; i++) {
-		for (int j = 0; j < delta3.width; j++) {
-			delta3.elements[j + i * delta3.width] = -delta3.elements[j + i * delta3.width];
-		}
-	}
-	
-
-	// NEED TO REMOVE BIAS FROM W2
-	for (int i = 0; i < W2.height; i++) {
-		for (int j = 1; j < W2.width; j++) {
-			if (j % (W2.width - 1) == 0) {
-				//printf("4th element will be removed\n\n");
+	for (int iterations = 0; iterations < 5000; iterations++) {
+		for (int i = 0; i < data.height; i++) {
+			for (int j = 0; j < data.width - 1; j++) {
+				input.elements[j] = data.elements[i * data.width + j];
 			}
+			target.elements[0] = data.elements[i * data.width + 4];
+			network = train(network, input, target);
 		}
 	}
 
-	// HAVE NOT INCLUDED ACTIVATION FUNCTION REMOVAL
-	Matrix W2T = matTranspose(W2);
-	Matrix delta2 = matMult(W2T, delta3);
+	input.elements[0] = 6.1;
+	input.elements[1] = 2.6;
+	input.elements[2] = 5.6;
+	input.elements[3] = 1.4;
 
-	// Calculating error gradient
-	Matrix inputT = matTranspose(input);
-	Matrix netT = matTranspose(net);
-
-	Matrix errGradientW1 = matMult(delta2, inputT);
-	Matrix errGradientW2 = matMult(delta3, netT);
-
-	// Updating weights using learning rate
-	errGradientW1.elements[0] = errGradientW1.elements[0] * 0.01;
-	errGradientW2.elements[0] = errGradientW2.elements[0] * 0.01;
-
-	W1 = matSub(W1, errGradientW1);
-	W2 = matSub(W2, errGradientW2);
-
-
-	std::vector<Matrix> weights;
-	weights.push_back(W1);
-	weights.push_back(W2);
-	
-	return weights;
-
-}
-
-
-
-Matrix feedForward(Matrix W1, Matrix W2, Matrix input) {
-	// Add bias to the input matrix
-	input.elements[input.height] = 1;
-	// Calculate output from hidden layer
-	Matrix net = matMult(W1, input);
-	// Sigmoid activation function
-	net = sigmoidFunction(net);
-	// Add bias to activation from hidden layer, add update matrix dimensions
-	net.elements[net.height] = 1;
-	net.height++;
-	// Calculate output from last layer
-	net = matMult(W2, net);
-	return net;
-
-}
-
-Matrix sigmoidFunction(Matrix input) {
-	for (int i = 0; i < input.height; i++) {
-		for (int j = 0; j < input.width; j++) {
-			input.elements[i * input.width + j] = 1 / (1+(exp(-input.elements[i * input.width + j])));
-		}
-	}
-	return input;
-}
-
-// Function to complete a matrix mulitplication on the host
-Matrix matMult(const Matrix A, const Matrix B) {
-	Matrix C;
-	C.height = A.height;
-	C.width = B.width;
-	C.elements = (float*)malloc(C.width * C.height * sizeof(float));
-
-	for (int i = 0; i < A.height; i++) {
-		for (int j = 0; j < B.width; j++) {
-			C.elements[j + i * B.width] = 0;
-
-			for (int k = 0; k < A.width; k++) {
-				C.elements[j + i * B.width] += A.elements[k + i * A.width] * B.elements[j + k * B.width];
-			}
-		}
-	}
-	return C;
-}
-
-Matrix matSub(const Matrix A, const Matrix B) {
-	Matrix C;
-	C.height = A.height;
-	C.width = A.width;
-	C.elements = (float*)malloc(C.width * C.height * sizeof(float));
-
-	for (int i = 0; i < C.height; i++) {
-		for (int j = 0; j < C.width; j++) {
-			C.elements[j + i * C.width] = A.elements[j + i * C.width] - B.elements[j + i * C.width];
-		}
-	}
-	return C;
-}
-
-Matrix matTranspose(const Matrix A) {
-	Matrix C;
-	C.height = A.width;
-	C.width = A.height;
-	C.elements = (float*)malloc(C.width * C.height * sizeof(float));
-
-	for (int row = 0; row < A.height; row++)
-		for (int col = 0; col < A.width; col++)
-			C.elements[row + col * A.height] = A.elements[col + row * A.width];
-
-	return C;
+	Matrix output = feedForward(network, input);
 }
